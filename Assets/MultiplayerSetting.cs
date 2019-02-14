4ae313d284fc7ee3539cd0610fe0a38dc694ddbb
yutakasaito <fvkibdcz7@yahoo.co.jp>
2019-02-14 19:13:16 +0900

    Ref: info_Gamer on youtube: How to make a Multiplayer Game in Unity - Photon 2 Plugin. Start Game
    浮ケ谷さん
    Youtubeのinfo_GamerさんのTutorialをやっています。
    画面がどうも同一になりません。
    色などはいいのですが、
    キューブであるプレハブのPhotonNetworkPlayerが
    なぜかtransform(x,y,z) =(182,102.5,0)になってしまいます。
    
    このTestでは、
    ①Buildしてひとつはそちらを実行する。バトルボタンを押す→キューブがでる
    ②もうひとつをUnityから起動する。バトルボタンを押す→ｷｭｰﾌﾞがでる
    ③MultiPlayerSceneでｷｭｰﾌﾞ（PhotonNetworkPlayer）が二つあることを確認する
    のですが、
    私が入力したこのプログラムでは
    ①Buildしてひとつはそちらを実行する。バトルボタンを押す→キューブがでない
    ②もうひとつをUnityから起動する。バトルボタンを押す→ｷｭｰﾌﾞがでない
    ③MultiPlayerSceneでｷｭｰﾌﾞ（PhotonNetworkPlayer）が二つあることは確認した
    という状態で、
    プレハブのPositionがおかしくなってしまっています。
    何か簡単な原因のような気がするのですが、できる範囲でかまいませんので、見ていただけないでしょうか？
    　　齋藤

---
+++ Assets/MultiplayerSetting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplayerSetting : MonoBehaviour
+{
+    public static MultiplayerSetting multiplayerSetting;
+
+    public bool delayStart; //false:contituous laodong game, true:call delay start game
+    public int maxPlayers;
+
+    public int menuScene;
+    public int multiplayerScene;
+
+    private void Awake()   //Singletonにする。ひとつしか、「この」ｲﾝｽﾀﾝｽを存在させない。無かったら「これ」を作り、あれば「これ」をDestroy
+    {
+        if (MultiplayerSetting.multiplayerSetting == null) {
+            MultiplayerSetting.multiplayerSetting = this;
+        }
+        else
+        {
+            if (MultiplayerSetting.multiplayerSetting != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);  //これって「this」がない場合があるんじゃないのか？
+    }
+}
---
+++ Assets/PhotonLobby.cs
@@ -0,0 +1,79 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+
+public class PhotonLobby : MonoBehaviourPunCallbacks
+{
+    public static PhotonLobby lobby;
+
+   public GameObject battleButton;
+   public GameObject cancelButton;
+
+    
+
+    private void Awake()
+    {
+        lobby = this; //Create the singleton, lives the Main menu scene;
+    }
+
+    // Start is called before the first frame update
+    void Start()    {
+        PhotonNetwork.ConnectUsingSettings(); //Connects to Master photon server
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Player has connected to the Photon master server");
+        PhotonNetwork.AutomaticallySyncScene = true;
+        battleButton.SetActive(true); //Player is now connected to servers, enable battlebutton to alow join a game
+    }
+
+    public void OnBattleButtonClicked()
+    {
+        Debug.Log("BattleButtonClicked");
+        battleButton.SetActive(false);
+        cancelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to join a random game but failed. There must be no open games available"); //Joinに失敗する理由は、利用できる部屋がない、が多いので
+        CreateRoom();
+    }
+
+    void CreateRoom()
+    {
+        Debug.Log("Trying to create a Room");
+        int randomRoomName = Random.Range(0, 10000);
+//        PhotonNetwork.CreateRoom(null, new RoomOptions());
+        RoomOptions roomOps = new RoomOptions() { IsVisible = true, IsOpen = true, MaxPlayers = (byte)MultiplayerSetting.multiplayerSetting.maxPlayers };
+        PhotonNetwork.CreateRoom("Room" + randomRoomName, roomOps);
+    }
+
+ //   public override void OnJoinedRoom()
+//    {
+ //       Debug.Log("We are now in a room");
+ //   }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Tried to create a new room but failed, there must already be a room with the same name");
+        CreateRoom();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCancelButtonClicked()
+    {
+        Debug.Log("CancelButton was Clicked");
+        cancelButton.SetActive(false);
+        battleButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+}
